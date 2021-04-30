13aae410d3fb631273c69ef4e3dc3d8481a3d027
Swumplurd <53883345+Swumplurd@users.noreply.github.com>
2021-04-30 01:42:24 -0500

    test con validaciones en manager/

--- manager/registro.js
+++ manager/registro.js
@@ -1,140 +1,50 @@
 $(document).ready(() => {
 
-    function valida_telefono() {
-        telefono = $('#registro-telefono').val();
-        telefono = parseInt(telefono);
-        if (telefono < 0) {
-            swal('Alerta en el telefono', 'No existen numeros de telefono NEGATIVOS', 'warning');
-            return false;
-        } else {
-            telefono = telefono.toString();
-            if (telefono.length < 10) {
-                swal('Alerta en el telefono', 'No debes de tener mas de 10 digitos\nRecuerda que en la zona metropolitana los inician con 55 o 56\nEjemplo: 5554189163', 'warning');
-                return false;
-            } else if (telefono.length > 10) {
-                swal('Alerta en el telefono', 'No debes de tener menos de 10 digitos\nRecuerda que en la zona metropolitana los inician con 55 o 56\nEjemplo: 5554189163', 'warning');
-                return false;
-            } else {
-                registro_telefono = $('#registro-telefono').val();
-                /* valida_seleccion_carrera(); */
+    class Validar {
+        constructor(nombre = $('#registro-nombre').val().toUpperCase(), ap_paterno = $('#registro-paterno').val().toUpperCase(), ap_materno = $('#registro-materno').val().toUpperCase(), fecha_nacimiento = $('#registro-nacimiento').val().split("-"), tel = $('#registro-telefono').val(), carrera = $('#registro-carrera').val(), email = $('#registro-email').val(), password = $('#registro-password').val(), re_password = $('#registro-password-confirmacion').val()) {
+            this.nombre = nombre,
+            this.ap_paterno = ap_paterno,
+            this.ap_materno = ap_materno,
+            this.fecha_nacimiento = fecha_nacimiento,
+            this.tel = tel,
+            this.carrera = carrera,
+            this.email = email,
+            this.password = password,
+            this.re_password = re_password
+        }
+        
+        vacios() {
+             if (this.nombre == '' || this.ap_paterno == '' || this.ap_materno == '' || this.fecha_nacimiento == '' || this.tel == '' || this.carrera == '' || this.email == '' || this.password == '' || this.re_password == '') {
+                 swal(":(", "Llena todos los campos", "error");
+             }
+        }
+
+        telefono() {
+            if (this.tel.length < 10 || this.tel.length > 10) {
+                swal(":(", "Tu telefono debe tener 10 digitos", "error");
             }
         }
-    }
 
-    function valida_fecha_nacimiento() {
-        if ($('#registro-nacimiento').val() != "") {
-            fecha_ingresada = $('#registro-nacimiento').val().split("-");
-            edad = 2021 - fecha_ingresada[0];
+        fecha() {
+            let fecha_actual = new Date();
+            let edad = fecha_actual.getFullYear() - this.fecha_nacimiento[0];
+            if ((fecha_actual.getMonth() < (this.fecha_nacimiento[1] - 1)) || (fecha_actual.getDate() < this.fecha_nacimiento[2] && fecha_actual.getMonth() == (this.fecha_nacimiento[1] - 1))) {
+                --edad;
+            }
             if (edad < 16) {
-                swal('Alerta en fecha de nacimiento', 'La fecha no es valida: Eres muy joven', 'warning');
+                swal("xD", "Estas muy bebé", "warning");
                 return false;
             } else if (edad > 99) {
-                swal('Alerta en fecha de nacimiento', 'La fecha no es valida: Eres muy viejo', 'warning');
+                swal(":O", "Estas muy... viejito", "warning");
                 return false;
-            } else {
-                registro_fecha_nacimiento = $('#registro-nacimiento').val();
-                valida_telefono();
             }
         }
     }
-
-    function valida_construccion_alfabetica() {
-        cadena = $('#registro-nombre').val();
-        console.log(cadena);
-        regexp1 = /[^A-Za-z +]/gi;
-        regexp2 = /[^A-Z]\s/gi;
-        resultado1 = cadena.match(regexp1);
-        resultado2 = cadena.match(regexp2);
-        console.log(resultado1);
-        console.log(resultado2);
-        if (resultado1 != null || resultado2 != null) {
-            if (resultado1 == null) {
-                resultado1 = "";
-            }
-            if (resultado2 == null) {
-                resultado2 = "";
-            }
-            swal('Alerta en el Nombre...', 'Los siguientes caracteres no son validos:\n\n' + resultado1 + resultado2, 'warning');
-            return false;
-        } else {
-            registro_nombre = $('#registro-nombre').val();
-            registro_nombre = registro_nombre.trim();
-            registro_nombre = registro_nombre.toUpperCase;
-            cadena = $('#registro-paterno').val();
-            resultado1 = cadena.match(regexp1);
-            resultado2 = cadena.match(regexp2);
-            if (resultado1 != null || resultado2 != null) {
-                if (resultado1 == null) {
-                    resultado1 = "";
-                }
-                if (resultado2 == null) {
-                    resultado2 = "";
-                }
-                swal('Alerta en el Apellido Paterno...', 'Los siguientes caracteres no son validos:\n\n' + resultado1 + resultado2, 'warning');
-                return false;
-            } else {
-                registro_paterno = $('#registro-paterno').val();
-                registro_paterno = registro_paterno.trim();
-                registro_paterno = registro_paterno.toUpperCase;
-                cadena = $('#registro-materno').val();
-                resultado1 = cadena.match(regexp1);
-                resultado2 = cadena.match(regexp2);
-                if (resultado1 != null || resultado2 != null) {
-                    if (resultado1 == null) {
-                        resultado1 = "";
-                    }
-                    if (resultado2 == null) {
-                        resultado2 = "";
-                    }
-                    swal('Alerta en el Apellido Materno...', 'Los siguientes caracteres no son validos:\n\n' + resultado1 + resultado2, 'warning');
-                    return false;
-                } else {
-                    registro_materno = $('#registro-materno').val();
-                    registro_materno = registro_materno.trim();
-                    registro_materno = registro_materno.toUpperCase;
-                    cadena = $('#registro-materno').val();
-                    resultado1 = cadena.match(regexp1);
-                    resultado2 = cadena.match(regexp2);
-                    valida_fecha_nacimiento();
-                }
-            }
-        }
-    }
-
-    function valida_vacios() {
-        if ($('#registro-nombre').val() == "") {
-            swal('Upps', 'Ingresa tu Nombre por favor', 'warning');
-            return false;
-        } else if ($('#registro-paterno').val() == "") {
-            swal('Upps', 'Ingresa tu Apellido Paterno por favor', 'warning');
-            return false;
-        } else if ($('#registro-materno').val() == "") {
-            swal('Upps', 'Ingresa tu Apellido Materno por favor', 'warning');
-            return false;
-        } else if ($('#registro-nacimiento').val() == "") {
-            swal('Upps', 'Ingresa tu Fecha de Nacimiento por favor', 'warning');
-            return false;
-        } else if ($('#registro-telefono').val() == "") {
-            swal('Upps', 'Ingresa tu Telefono por favor', 'warning');
-            return false;
-        } else if ($('#registro-carrera').val() == "") {
-            swal('Upps', 'Selecciona tu Carrera, por favor', 'warning');
-            return false;
-        } else if ($('#registro-email').val() == "") {
-            swal('Upps', 'Ingresa tu Email por favor', 'warning');
-            return false;
-        } else if ($('#registro-password').val() == "") {
-            swal('Upps', 'Ingresa tu Password por favor', 'warning');
-            return false;
-        } else if ($('#registro-password-confirmacion').val() == "") {
-            swal('Upps', 'Confirma tu Password por favor', 'warning');
-            return false;
-        } else {
-            valida_construccion_alfabetica();
-        }
-    }
-
+    
     $('#btn-registro-usuario').click(() => {
-        valida_vacios();
+        let validar = new Validar();
+        validar.vacios();
+        validar.telefono();
+        validar.fecha();
     });
 });
--- view/registro.php
+++ view/registro.php
@@ -6,30 +6,30 @@
           <legend>Informacion Personal</legend>
           <div class="form-group">
             <label for="registro-nombre">Nombre</label>
-            <input type="text" name="registro-nombre" id="registro-nombre" class="form-control" placeholder="" aria-describedby="helpId">
+            <input required oncopy="return false" oncontextmenu="return false" onkeydown="return noCtrl(event)" onkeypress="return onlyLeters(event)" type="text" style="text-transform:uppercase" name="registro-nombre" id="registro-nombre" class="form-control" placeholder="" aria-describedby="helpId">
             <small id="helpId" class="text-muted">Ingresa tu(s) nombre(s)</small>
           </div>
           <div class="form-group row">
             <div class="col">
               <label for="registro-paterno">Apellido Paterno</label>
-              <input type="text" name="registro-paterno" id="registro-paterno" class="form-control" placeholder="" aria-describedby="helpId">
+              <input required oncopy="return false" oncontextmenu="return false" onkeydown="return noCtrl(event)" onkeypress="return onlyLeters(event)" type="text" style="text-transform:uppercase" name="registro-paterno" id="registro-paterno" class="form-control" placeholder="" aria-describedby="helpId">
               <small id="helpId" class="text-muted">Ingresa tu apellido paterno</small>
             </div>
             <div class="col">
               <label for="registro-materno">Apellido Materno</label>
-              <input type="text" name="registro-materno" id="registro-materno" class="form-control" placeholder="" aria-describedby="helpId">
+              <input required oncopy="return false" oncontextmenu="return false" onkeydown="return noCtrl(event)" onkeypress="return onlyLeters(event)" type="text" style="text-transform:uppercase" name="registro-materno" id="registro-materno" class="form-control" placeholder="" aria-describedby="helpId">
               <small id="helpId" class="text-muted">Ingresa tu apellido materno</small>
             </div>
           </div>
           <div class="form-group row">
             <div class="col">
               <label for="registro-nacimiento">Fecha de Nacimiento</label>
-              <input type="date" name="registro-nacimiento" id="registro-nacimiento" class="form-control" placeholder="" aria-describedby="helpId">
+              <input required oncopy="return false" oncontextmenu="return false" onkeydown="return noCtrl(event)" type="date" name="registro-nacimiento" id="registro-nacimiento" class="form-control" placeholder="" aria-describedby="helpId">
               <small id="helpId" class="text-muted">Ingresa tu fecha de nacimiento</small>
             </div>
             <div class="col">
               <label for="registro-telefono">Telefono de Contacto</label>
-              <input type="tel" name="registro-telefono" id="registro-telefono" class="form-control" placeholder="" aria-describedby="helpId">
+              <input required oncopy="return false" oncontextmenu="return false" onkeydown="return noCtrl(event)" onkeypress="return onlyNumbers(event)" type="tel" name="registro-telefono" id="registro-telefono" class="form-control" placeholder="" aria-describedby="helpId">
               <small id="helpId" class="text-muted">Preferentemente celular</small>
             </div>
           </div>
@@ -52,18 +52,18 @@
           <legend>Datos para Iniciar Sesion</legend>
           <div class="form-group">
             <label for="registro-email">Email</label>
-            <input type="email" name="registro-email" id="registro-email" class="form-control" placeholder="" aria-describedby="helpId">
+            <input required oncopy="return false" oncontextmenu="return false" onkeydown="return noCtrl(event)" onkeypress="return onlyEmail(event)" type="email" name="registro-email" id="registro-email" class="form-control" placeholder="" aria-describedby="helpId">
             <small id="helpId" class="text-muted">Ingresa un correo valido</small>
           </div>
           <div class="form-group row">
             <div class="col">
               <label for="registro-password">Password</label>
-              <input type="password" name="registro-password" id="registro-password" class="form-control" placeholder="" aria-describedby="helpId">
+              <input required oncopy="return false" oncontextmenu="return false" onkeydown="return noCtrl(event)" type="password" name="registro-password" id="registro-password" class="form-control" placeholder="" aria-describedby="helpId">
               <small id="helpId" class="text-muted">Ingresa una contraseña</small>
             </div>
             <div class="col">
               <label for="registro-password-confirmacion">Confirmar Password</label>
-              <input type="password" name="registro-password-confirmacion" id="registro-password-confirmacion" class="form-control" placeholder="" aria-describedby="helpId">
+              <input required oncopy="return false" oncontextmenu="return false" onkeydown="return noCtrl(event)" type="password" name="registro-password-confirmacion" id="registro-password-confirmacion" class="form-control" placeholder="" aria-describedby="helpId">
               <small id="helpId" class="text-muted">Confirma tu contraseña</small>
             </div>
           </div>
@@ -78,5 +78,49 @@
     </div>
   </div>
 </div>
+
+<script>
+  function onlyLeters(event) {
+    let key = event.keyCode || event.which;
+    let tecla = String.fromCharCode(key).toLowerCase();
+    let letras = "abcdefghijklmnopqrstuvwxyzñáéíóú ";
+    if (letras.indexOf(tecla) == -1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function onlyNumbers(event) {
+    let key = event.keyCode || event.which;
+    let tecla = String.fromCharCode(key).toLowerCase();
+    let numeros = "0123456789";
+    console.log(numeros.indexOf(tecla));
+    if (numeros.indexOf(tecla) == -1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function onlyEmail(event) {
+    let key = event.keyCode || event.which;
+    let tecla = String.fromCharCode(key).toLowerCase();
+    let chars = "0123456789.abcdefghijklmnopqrstuvwxyz@_-";
+    if (chars.indexOf(tecla) == -1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function noCtrl(event) {
+    if (event.ctrlKey && (event.keyCode === 67 || event.keyCode === 86 || event.keyCode === 85 || event.keyCode === 117)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+</script>
 
 <script src="manager/registro.js"></script>
